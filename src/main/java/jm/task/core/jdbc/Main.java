f8a5666ce5ba8a4a0d228c29d0535ff25f8771d5
Varuzhan Manucharyan <varuzh.manucharian@yandex.ru>
2025-05-14 13:02:52 +0300

    Добавлен .gitignore и удалены файлы из индекса

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,14 +1,11 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.ConfigUtil;
-
-
 import java.util.List;
 import java.util.logging.Logger;
 
